5a17ef3e8d9c0d0760ae4eddf74fde2bff5f2a44
PAPANABOINA <223024691@ge.com>
2021-09-29 09:59:13 +0530

    Adding files for creating pythonapp with helm-k8s-s3-terraform

--- main.tf
+++ main.tf
@@ -3,10 +3,12 @@ module "qa" {
   s3_bucket_name = "qa-gopibhaskar-papanaboina-platform-challenge"
   namespace_name = "qa-ns"
   environment = "qa"
+  clusterid = "minikube"
 }
 module "staging" {
   source = "./modules/"
   s3_bucket_name = "staging-gopibhaskar-papanaboina-platform-challenge"
   namespace_name = "staging-ns"
   environment = "staging"
+  clusterid = "minikube"
 }
